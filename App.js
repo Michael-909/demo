25e8bc3e8edfd02a8ca40ff32039faf4af654017
Thomas Wingo <santa960727@gmail.com>
2020-11-24 15:26:47 -0600

    package reinstall

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import NavigationContainer from './app/navigations/NavigationContainer';
+import store from './app/redux/store';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+		<Provider store={store}>
+			<NavigationContainer />
 			<StatusBar style="auto" />
-		</View>
+		</Provider>
 	);
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+};
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,3 +1,10 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import ReduxThunk from 'redux-thunk';
 
+import bikeReducer from './reducers/bike';
+
+const rootReducer = combineReducers({
+	bike: bikeReducer
+});
+
+export default store = createStore(rootReducer, applyMiddleware(ReduxThunk));
